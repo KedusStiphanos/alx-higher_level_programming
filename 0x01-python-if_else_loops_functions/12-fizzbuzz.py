6371388b065be299d79bcf1502e1eb93fb9ca057
KedusStiphanos <101466079+KedusStiphanos@users.noreply.github.com>
2022-07-17 19:10:17 +0300

    Update 12-fizzbuzz.py

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def fizzbuzz():
     s = ''
-    for n in range(1,101):
+    for n in range(1, 101):
         if n % 3 == 0 and n % 5 == 0:
             s = "FizzBuzz"
         elif n % 3 == 0:
